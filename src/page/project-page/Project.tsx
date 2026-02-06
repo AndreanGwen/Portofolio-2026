10eb2c795f4c9090c92f1866393cced2841d5282
AndreanGwen <andreansatriya22@gmail.com>
2026-02-06 19:55:33 +0700

    add project page

--- src/page/project-page/Project.tsx
+++ src/page/project-page/Project.tsx
@@ -1,7 +1,10 @@
 "use client";
 import Navbar from "@/common/navbar/Navbar";
 import axios from "axios";
+import Image from "next/image";
+import Link from "next/link";
 import { useEffect, useState } from "react";
+import { FiGithub } from "react-icons/fi";
 
 export default function Project() {
   const [projects, setProjects] = useState([]);
@@ -10,7 +13,7 @@ export default function Project() {
     const fetchData = async () => {
       try {
         const url = process.env.NEXT_PUBLIC_PROJECT_LIST!;
-        const res = await axios.get(url);
+        const res = await axios.get(url + "project-list");
         setProjects(res.data.data);
       } catch (error) {
         console.error(error);
@@ -19,6 +22,8 @@ export default function Project() {
 
     fetchData();
   }, []);
+
+  console.log(projects);
 
   return (
     <>
@@ -31,6 +36,68 @@ export default function Project() {
           </span>
           <span className={`hover:text-black cursor-pointer`}>Website</span>
           <span className={`hover:text-black cursor-pointer`}>IOT</span>
+        </div>
+
+        <div
+          className={`w-full h-fit pr-24 pl-24 pt-7 flex flex-wrap justify-between`}
+        >
+          {projects.length != 0 ? (
+            projects.map((items: any, index: any) => (
+              <div
+                key={index}
+                className="group w-96 h-44 border-2 border-b-8 border-blue-950 rounded-xl relative overflow-hidden"
+              >
+                <Image
+                  alt="img-project"
+                  src={items?.projectImage}
+                  fill
+                  className="object-cover"
+                />
+
+                <Link
+                  href={items?.projectLink}
+                  className="absolute right-1 top-1 hidden w-9 h-9 bg-[#80d8da] rounded-full group-hover:flex justify-center items-center border-2 border-blue-950"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <FiGithub size={20} />
+                </Link>
+
+                <div
+                  className="
+                absolute bottom-0 left-0 w-full
+                bg-[#80d8da]
+                z-10
+                flex flex-col
+                px-3
+                border-t-2 border-blue-950
+                transition-all duration-300
+                h-10 pt-1.5
+                group-hover:h-20 
+              "
+                >
+                  <h1 className="text-black text-md font-semibold">
+                    {items?.projectName}
+                  </h1>
+
+                  <p
+                    className="
+                  text-xs text-black
+                  opacity-0
+                  translate-y-2
+                  transition-all duration-300
+                  group-hover:opacity-100
+                  group-hover:translate-y-0
+                "
+                  >
+                    {items?.projectDetail}
+                  </p>
+                </div>
+              </div>
+            ))
+          ) : (
+            <>Loading</>
+          )}
         </div>
       </div>
     </>
