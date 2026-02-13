431c8848e582f47ec15350ee2d6fa1a2614033e5
AndreanGwen <andreansatriya22@gmail.com>
2026-02-13 17:34:06 +0700

    add hover mobile

--- src/common/capsul/Capsul.tsx
+++ src/common/capsul/Capsul.tsx
@@ -2,7 +2,7 @@ const Capsul = (props: any) => {
   const { desc, color } = props;
   return (
     <div
-      className={`text-xs text-gray-600 flex items-center justify-center ${color} w-fit px-2.5 py-0.5 rounded-full hover:scale-105 transition font-semibold select-none`}
+      className={`text-xs text-gray-600 flex items-center justify-center ${color} w-fit px-2.5 py-0.5 rounded-full hover:scale-105 transition font-semibold select-none active:scale-95`}
     >
       {desc}
     </div>
